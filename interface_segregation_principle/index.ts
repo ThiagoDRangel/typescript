d3c057ebabd697ce698142df1de53817c25e09fc
ThiagoDRangel <info.tec.campos@gmail.com>
2023-06-02 16:01:52 -0300

    Finalizando

--- interface_segregation_principle/index.ts
+++ interface_segregation_principle/index.ts
@@ -11,33 +11,43 @@ class ShippedItem {
 }
 
 class Flight {
-  constructor(public num: number, public passengers: Passenger[]) { }
+  // Primeiro, ajustamos o construtor da classe Flight
+  // para suportar tanto passageiros quanto cargas
+  constructor(public num: number, public payload: (Passenger | ShippedItem)[]) { }
 
-  add(newPassenger: Passenger): void {
-    this.passengers.push(newPassenger); 
+  // A função add agora poderá adicionar tanto
+  // passageiros quanto cargas
+  add(newPayload: Passenger | ShippedItem): void {
+    this.payload.push(newPayload); 
   }
 
-  remove(removedPassenger: Passenger): void {
-    const index = this.passengers.indexOf(removedPassenger, 0);
+  // De igual modo, a função remove pode remover
+  // tanto passageiros quanto cargas
+  remove(removedPayload: Passenger | ShippedItem): void {
+    const index = this.payload.indexOf(removedPayload, 0);
     if (index > -1) {
-      this.passengers.splice(index, 1);
+      this.payload.splice(index, 1);
     }
   }
 }
 
 class Company {
   constructor(public nome: string, public flights: Flight[]) { }
-  newFlight(flightNum: number): void { }
-  addToFlight(flightNum: number, passenger: Passenger): void { }
-  removeFromFlight(flightNum: number, passenger: Passenger): void { }
-}
-
-class TravelingCompany extends Company {
+  // newFlight pode ser implementada aqui, já que
+  // sua funcionalidade se mantém em todas as
+  // subclasses
   newFlight(flightNum: number): void {
     const newFlight = new Flight(flightNum, []);
     this.flights.push(newFlight);
   }
 
+  // Ajustes feitos na superclasse são transmitidos
+  // para as subclasses
+  addToFlight(flightNum: number, payload: Passenger | ShippedItem): void { }
+  removeToFlight(flightNum: number, payload: Passenger | ShippedItem): void { }
+}
+
+class TravelingCompany extends Company {
   addToFlight(flightNum: number, passenger: Passenger): void {
     const currentFlight = this.flights.find((f) => f.num === flightNum);
     if (currentFlight) {
@@ -54,15 +64,17 @@ class TravelingCompany extends Company {
 }
 
 class ShippingCompany extends Company {
-  newFlight(flightNum: number): void {
-    const newFlight = new Flight(flightNum, []);
-    this.flights.push(newFlight);
-  }
-
-  addToFlight(flightNum: number, item: ShippedItem | Passenger): void {
+  addToFlight(flightNum: number, item: ShippedItem): void {
     const currentFlight = this.flights.find((f) => f.num === flightNum);
     if (currentFlight) {
       currentFlight.add(item);
+    }
+  }
+
+  removeToFlight(flightNum: number, item: ShippedItem): void {
+    const currentFlight = this.flights.find((f) => f.num === flightNum);
+    if (currentFlight) {
+      currentFlight.remove(item);
     }
   }
 }
