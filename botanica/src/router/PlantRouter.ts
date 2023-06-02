dd99640047908c3fe8feb00582140aab6238a035
ThiagoDRangel <info.tec.campos@gmail.com>
2023-06-02 13:58:09 -0300

    Refatorando

--- botanica/src/router/PlantRouter.ts
+++ botanica/src/router/PlantRouter.ts
@@ -1,5 +1,11 @@
 import { Router } from 'express';
 import PlantController from '../controllers/PlantController';
+import PlantModel from '../models/PlantModel';
+import PlantService from '../services/PlantService';
+
+const plantModel = new PlantModel();
+const plantService = new PlantService(plantModel);
+const plantController = new PlantController(plantService);
 
 const plantController = new PlantController();
 
@@ -7,5 +13,8 @@ const plantRouter = Router();
 
 plantRouter.get('/', (req, res, next) => plantController.getAll(req, res, next));
 plantRouter.post('/', (req, res, next) => plantController.create(req, res, next));
+plantRouter.get('/:id', (req, res, next) => plantController.getById(req, res, next));
+plantRouter.delete('/:id', (req, res, next) => plantController.remove(req, res, next));
+plantRouter.put('/:id', (req, res, next) => plantController.update(req, res, next));
 
 export default plantRouter;
