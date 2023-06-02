68890152e0b8856632c54458e1121e1f4ae8c47f
ThiagoDRangel <info.tec.campos@gmail.com>
2023-06-02 14:04:15 -0300

    Finalizando

--- botanica/src/router/PlantRouter.ts
+++ botanica/src/router/PlantRouter.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express';
+
 import PlantController from '../controllers/PlantController';
-import PlantModel from '../models/PlantModel';
+import Mysql2PlantModel from '../models/Mysql2PlantModel';
 import PlantService from '../services/PlantService';
 
-const plantModel = new PlantModel();
+const plantModel = new Mysql2PlantModel();
 const plantService = new PlantService(plantModel);
 const plantController = new PlantController(plantService);
-
-const plantController = new PlantController();
 
 const plantRouter = Router();
 
